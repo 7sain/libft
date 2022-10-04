a667d9abeda57f627b8a0888ae0a9e21af03d1bd
7sain <x.7sain@hotmail.com>
2022-10-04 22:24:38 +0400

    123

---  ft_striteri.c
+++  ft_striteri.c
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*    ft_striteri.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hualhash <hualhash@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/02 15:33:10 by hualhash          #+#    #+#             */
-/*   Updated: 2022/10/04 21:51:21 by hualhash         ###   ########.fr       */
+/*   Created: 2022/10/04 21:52:46 by hualhash          #+#    #+#             */
+/*   Updated: 2022/10/04 22:18:45 by hualhash         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
+	unsigned int i;
+
+	if (!s || !f)
+	return;
+	i = 0;
+	while (s[i])
+	{
+		f(i, s + i);
+		i++;
+	}
 	
 }
